b727a1b246fb2fab73bf2ddb293707d36d728124
Ashish Upadhyay <ashishup1999@gmail.com>
2024-04-14 18:48:09 +0530

    style changes

--- src/app/ContentSection.tsx
+++ src/app/ContentSection.tsx
@@ -10,40 +10,49 @@ const ContentSection = ({
   children: ReactNode;
   setClickedMenu: Function;
 }) => {
-
   const onScroll = () => {
+    const areaByDiv = [0, 0, 0, 0];
     const ab = document.querySelector(".about-wrapper");
     if (ab) {
-      const { top } = ab?.getBoundingClientRect();
-      if (top > 0 && top < (window.innerHeight * 2) / 3) {
-        setClickedMenu(MENU.ABOUT);
-        return;
+      const { top, bottom } = ab?.getBoundingClientRect();
+      if (top > window.innerHeight || bottom < 0) {
+        areaByDiv[0] = 0;
+      } else {
+        areaByDiv[0] = Math.min(window.innerHeight, bottom) - Math.max(0, top);
       }
     }
     const exp = document.querySelector(".exp-wrapper");
     if (exp) {
-      const { top } = exp?.getBoundingClientRect();
-      if (top > 0 && top < (window.innerHeight * 2) / 3) {
-        setClickedMenu(MENU.EXPERIENCE);
-        return;
+      const { top, bottom } = exp?.getBoundingClientRect();
+      if (top > window.innerHeight || bottom < 0) {
+        areaByDiv[1] = 0;
+      } else {
+        areaByDiv[1] = Math.min(window.innerHeight, bottom) - Math.max(0, top);
       }
     }
     const proj = document.querySelector(".projects");
     if (proj) {
-      const { top } = proj?.getBoundingClientRect();
-      if (top > 0 && top < (window.innerHeight * 2) / 3) {
-        setClickedMenu(MENU.PROJECTS);
-        return;
+      const { top, bottom } = proj?.getBoundingClientRect();
+      if (top > window.innerHeight || bottom < 0) {
+        areaByDiv[2] = 0;
+      } else {
+        areaByDiv[2] = Math.min(window.innerHeight, bottom) - Math.max(0, top);
       }
     }
     const achv = document.querySelector(".achvs-wrapper");
     if (achv) {
-      const { top } = achv?.getBoundingClientRect();
-      if (top > 0 && top < (window.innerHeight * 2) / 3) {
-        setClickedMenu(MENU.ACHIEVEMENT);
-        return;
+      const { top, bottom } = achv?.getBoundingClientRect();
+      if (top > window.innerHeight || bottom < 0) {
+        areaByDiv[3] = 0;
+      } else {
+        areaByDiv[3] = Math.min(window.innerHeight, bottom) - Math.max(0, top);
       }
     }
+    const mxVal = Math.max(...areaByDiv);
+    if (mxVal === areaByDiv[0]) setClickedMenu(MENU.ABOUT);
+    if (mxVal === areaByDiv[1]) setClickedMenu(MENU.EXPERIENCE);
+    if (mxVal === areaByDiv[2]) setClickedMenu(MENU.PROJECTS);
+    if (mxVal === areaByDiv[3]) setClickedMenu(MENU.ACHIEVEMENT);
   };
 
   return (
